e137dff68709c777af37e6221dd53dc56c5c228a
alicio <alicio@dynamix.com.br>
2019-08-02 11:22:00 -0300

    implements methods mysql connection

--- src/main/java/br/com/proway/api/data/ConexaoMysqlJDBC.java
+++ src/main/java/br/com/proway/api/data/ConexaoMysqlJDBC.java
@@ -1,5 +1,52 @@
 package br.com.proway.api.data;
 
-public class ConexaoMysqlJDBC {
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ConexaoMysqlJDBC implements ConexaoJDBC {
+
+	private Connection connection = null;
+
+	public ConexaoMysqlJDBC() {
+
+	}
+
+	@Override
+	public Connection getConnection() {
+		return this.connection;
+	}
+
+	@Override
+	public void close() {
+		if (this.connection != null) {
+			try {
+				this.connection.close();
+			} catch (SQLException ex) {
+				Logger.getLogger(ConexaoMysqlJDBC.class.getName()).log(Level.SEVERE, null, ex);
+			}
+		}
+	}
+
+	@Override
+	public void commit() throws SQLException {
+        this.connection.commit();
+        this.close();
+	}
+
+	@Override
+	public void rollback() {
+        if (this.connection != null) {
+            try {
+                this.connection.rollback();
+            } catch (SQLException ex) {
+                Logger.getLogger(ConexaoMysqlJDBC.class.getName()).log(Level.SEVERE, null, ex);
+            } finally {
+                this.close();
+            }
+        }
+    }
+	}
 
 }
