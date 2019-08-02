2bf70f5f680cbfb71380eec85ea534c40a9409d7
alicio <alicio.fcn@gmail.com>
2019-08-02 13:22:25 -0300

    controller hello

--- src/main/java/br/com/proway/api/MyApplication.java
+++ src/main/java/br/com/proway/api/MyApplication.java
@@ -6,9 +6,9 @@ import org.glassfish.jersey.server.ResourceConfig;
 
 @ApplicationPath("rest")
 public class MyApplication extends ResourceConfig {
-	
+
 	public MyApplication() {
-		packages("br.com.proway.api.controllers");
+		packages("br.com.proway.api.controller");
 	}
 
 }
