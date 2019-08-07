3984c6bb975269b5c956f1e265f5c2acd3173fba
alicio <alicio@dynamix.com.br>
2019-08-07 16:00:34 -0300

    config dbmysql

--- src/main/java/br/com/proway/api/data/ConexaoMysqlJDBC.java
+++ src/main/java/br/com/proway/api/data/ConexaoMysqlJDBC.java
@@ -16,12 +16,25 @@ public class ConexaoMysqlJDBC implements ConexaoJDBC {
 	private static final String PASSWORD = "123456";
 
 	// Dados de caminho, porta e nome da base de dados que irá ser feita a conexão
-	private static final String DATABASE_URL = "jdbc:mysql:127.0.0.1:3306/exemplo_jdbc";
+	private static final String DATABASE_URL = "jdbc:mysql://0.0.0.0:3306/exemplo_jdbc";
 
 	public ConexaoMysqlJDBC() throws SQLException, ClassNotFoundException {
-		Class.forName("com.mysql.jdbc.Driver");
+		Class.forName("com.mysql.jdbc.Driver"); 
 		this.connection = DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD);
 		this.connection.setAutoCommit(false);
+		
+		//Testa sua conexão//  
+		 
+        if (connection != null) {
+ 
+            System.out.println("Conectou");
+ 
+        } else {
+ 
+        	System.out.println("STATUS--->Não foi possivel realizar conexão");
+ 
+        }
+		
 	}
 
 	@Override
